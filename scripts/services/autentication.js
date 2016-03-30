1d679f2cea33f0f2446d1813706e2833d2e8e6c1
Kevin Oliva <kevin.oliva@babel.es>
2016-03-30 10:31:14 +0200

    Servicio tonto de login

---
+++ scripts/services/autentication.js
@@ -0,0 +1,20 @@
+angular.module("pelisbabel", []).service("autentication", ["$log", function($log){
+	
+	var userLogin = [false, ""];
+
+	this.setLogin = function(user){
+		
+		userLogin[0] = true;
+		userLogin[1] = user;
+		return userLogin;
+
+	};
+
+	this.setLogin = function(user){
+		
+		return userLogin;
+
+	};
+
+	
+}]);
