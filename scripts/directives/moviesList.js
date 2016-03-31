17f80d5536221fdcc807ec6c5645863e37fa8cc9
Kevin Oliva <kevin.oliva@babel.es>
2016-03-31 10:42:21 +0200

    Errata al sincronizar

--- scripts/directives/moviesList.js
+++ scripts/directives/moviesList.js
@@ -3,12 +3,8 @@ angular.module("pelisbabel").directive("moviesList", function(){
 		restrict:"AE",
 		scope: {
 			model:"=items",
-<<<<<<< HEAD
-			modo:"@"
-=======
+			modo:"@",
 			usuario:"="
-			
->>>>>>> b25842097d53f1f128775d8b5887019fe769a479
 		},
 		templateUrl:"views/moviesList.html"
 	};
